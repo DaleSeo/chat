7e2202a5a9c2cd71af5f06f10a1e71f82ccb56b2
Dale Seo <dale.seo@gmail.com>
2016-12-22 09:36:03 +0900

    add a routher for /test

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.get('/test', function(req, res){
+  res.send('Test!');
+});
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
