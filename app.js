5d40aa5599596858212e96deb7be69d08f18802e
Dale Seo <dale.seo@gmail.com>
2017-02-13 22:51:56 +0900

    direct message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-let users = new Set();
+let users = {};
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -20,32 +20,51 @@ io.on('connection', (socket) => {
 	console.log('a user connected');
 
   socket.on('add user', (username, callback) => {
-    if (users.has(username)) {
+    if (username in users) {
       console.log('duplicated user!');
       callback(false);
     } else {
       console.log(username, ' has logged in');
       callback(true);
       socket.username = username;
-      users.add(username);
-      io.emit('users', Array.from(users));
+      users[socket.username] = socket;
+      io.emit('users', Object.keys(users));
       socket.emit('login', {
         username: username,
-        numUsers: users.size
+        numUsers: Object.keys(users).length
       });
       socket.broadcast.emit('user joined', {
         username: username,
-        numUsers: users.size
+        numUsers: Object.keys(users).length
       });
     }
   });
 
-  socket.on('send message', (msg) => {
+  socket.on('send message', (msg, callback) => {
 		console.log(`message: ${msg} from ${socket.username}`);
-		socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: msg
-    });
+    msg = msg.trim();
+    if (msg.substr(0, 3) === '/w ') {
+      msg = msg.substr(3);
+      let idx = msg.indexOf(' ');
+      if (idx > -1) {
+        let username = msg.substr(0, idx);
+        msg = msg.substr(idx + 1);
+        if (username in users) {
+          users[username].emit('direct message', {
+            username: socket.username,
+            message: msg
+          });
+          console.log('Whispering...');
+        }
+      } else {
+        callback('Please enter a message to send a direct message.');
+      }
+    } else {
+      socket.broadcast.emit('new message', {
+        username: socket.username,
+        message: msg
+      });
+    }
 	});
 
   socket.on('typing', () => {
@@ -62,12 +81,12 @@ io.on('connection', (socket) => {
 
 	socket.on('disconnect', () => {
     console.log('user disconnected');
-    if (!socket.nickname) return;
-    users.delete(socket.username);
-    io.emit('users', Array.from(users));
+    if (!socket.username) return;
+    delete users[socket.username];
+    io.emit('users', Object.keys(users));
     socket.broadcast.emit('user left', {
       username: socket.username,
-      numUsers: users.size
+      numUsers: Object.keys(users).length
     });
   });
 });
